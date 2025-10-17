18ed2a4b173ba5cf60189f8268a99d9a5c73807f
LENOVO <anjanaheshan676@gmail.com>
2025-10-17 15:36:10 +0530

    Add input fields (From , To , Date)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import Hero from './hero/Hero'
 
 const Home = () => {
   return (
     <>
-      <div className='h-screen w-full flex items-center justify-center'> 
-        <h1 className="text-5xl text-neutral-950 font-bold">
-          This is Home Page Section ...
-        </h1>  
-      </div>
-
-      <div className='h-screen w-full flex items-center justify-center'> 
+      <div className='space-y-16 w-full min-h-screen pb-16'>
+       {/* Hero */}
+        {/* <div className='h-screen w-full flex items-center justify-center'> 
+            <h1 className="text-5xl text-neutral-950 font-bold">
+            This is Home Page Section ...
+            </h1>  
+        </div> */}
+        
+        {/* About */}
+      {/* <div className='h-screen w-full flex items-center justify-center'> 
         <h1 className="text-5xl text-neutral-950 font-bold">
           This is About Page Section ...
         </h1>  
+      </div> */}
+
+      <Hero />
+
       </div>
     </>
   )
